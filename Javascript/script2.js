286dc96ae2f798790aabd983307051be15e3f9e8
Kyle Cheung <kcheu4@uwo.ca>
2018-04-20 23:21:00 -0400

    javascript examples

---
+++ Javascript/script2.js
@@ -0,0 +1,12 @@
+//examine the document object //
+
+//console.dir(document);
+
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+//document.title = 123;
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
