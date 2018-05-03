debee92becd62f4f177654c2d6ba4f4d41b3ea4b
Kyle Cheung <kcheu4@uwo.ca>
2018-05-02 23:16:54 -0400

    javascript exercises

--- Javascript/script2.js
+++ Javascript/script2.js
@@ -10,3 +10,10 @@ console.log(document.doctype);
 console.log(document.head);
 console.log(document.body);
 console.log(document.all);
+
+//console.log(document.getElementById("test"));
+
+var par = document.getElementById('test');
+
+console.log(par);
+par.innerText = 'innerHTML';
