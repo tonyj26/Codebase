83ba1175826404294012cdbbe7e0395a3f3ce8a5
Kyle Cheung <kcheu4@uwo.ca>
2018-05-17 20:26:07 -0400

    added more react examples

---
+++ Javascript/React/hello-world/src/clock.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <Clock />
+      <Clock />
+      <Clock />
+    </div>
+    );
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+      );
+  }
+}
+
+ReactDom.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
+
--- Javascript/React/hello-world/src/index.js
+++ Javascript/React/hello-world/src/index.js
@@ -2,7 +2,31 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+      );
+  }
+}
+
 ReactDom.render(
-  <h1>hello, world!</h1>,
+  <Toggle />,
   document.getElementById('root')
 );
+
