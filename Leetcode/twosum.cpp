407eaf5760243d8bf8516367f27fb3b6555051cf
Ky Le <cheung.kyle1@gmail.com>
2018-10-06 20:00:45 -0400

    standalone solution

--- Leetcode/twosum.cpp
+++ Leetcode/twosum.cpp
@@ -1,4 +1,5 @@
 /*
+LEETCODE 1
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -11,6 +12,7 @@ return [0, 1].
 #include <vector>
 #include <iostream>
 #include <unordered_map>
+#include <string>
 
 class Solution {
 public:
@@ -30,11 +32,30 @@ public:
     }
     return result;
   }
+
+  std::string integerVectorToString(std::vector<int> list, int length = -1) {
+    if (length == -1) {
+      length = list.size();
+    }
+
+    if (length == 0) {
+      return "[]";
+    }
+
+    std::string result;
+    for(int index = 0; index < length; index++) {
+      int number = list[index];
+      result += std::to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+  }
 };
 
 int main(){
   std::vector<int> nums = {2, 7, 11, 15};
   Solution sol;
-  sol.twoSum(nums, 9).empty();
+  std::vector<int> ret = sol.twoSum(nums, 9);
+
+  std::cout << sol.integerVectorToString(ret);
   return 0;
 }
