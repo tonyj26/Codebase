411df2c74e6bd824e14103016cdf2a66e0235a94
Kyle Cheung <kcheu4@uwo.ca>
2018-10-14 12:57:17 -0400

    bug fixes

--- C/assignments/skeleton_code/launch.c
+++ C/assignments/skeleton_code/launch.c
@@ -4,9 +4,7 @@ short launch(char **args)
 {
   short status;
   int fd[2];
-  int piped = 0;
   pid_t pid, wpid;
-  char input[64], output[64];
 
 
   //pipe before forking
@@ -15,22 +13,9 @@ short launch(char **args)
   }
 
   pid = fork();
-  for (int i = 0; args[i] != NULL; i++){
-    if(strcmp(args[i], "|") == 0){
-      args[i] = NULL;
-      int piped = 2;
-    }
-  }
-
 
   //child
   if(pid == 0){
-    if (piped){
-      if(dup2(fd[0], STDIN_FILENO)<0) {
-        perror("can't dup");
-        exit(1);
-      }
-    }
 
     if(execvp(args[0], args) == -1){
       perror("bash");
@@ -42,15 +27,6 @@ short launch(char **args)
   }
   else {
     //parent
-    if(piped){
-      if(dup2(fd[1], STDOUT_FILENO) < 0){
-        perror("can't dup");
-        exit(1);
-      }
-    }
-//    if(execvp(args[0], args) == -1){
-  //    perror("bash");
-   // }
     do {
       int childstatus;
       wpid = waitpid(pid, &childstatus, 0);
