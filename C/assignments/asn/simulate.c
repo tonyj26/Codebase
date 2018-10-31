0d0a3f660185544b34de8db597d95cf7fd3156e0
Kyle Cheung <kcheu4@uwo.ca>
2018-10-31 18:27:53 -0400

    updated ass 2

--- C/assignments/asn/simulate.c
+++ C/assignments/asn/simulate.c
@@ -14,20 +14,21 @@ void simulate(int memory_value, linked_stack_t *stack)
 {
   int i;
   max_memory = memory_value;
-
-
+  fp = fopen(SYSTEM_OUTPUT, "ab+");
   pthread_t thread[NUMBER_OF_THREADS];
 
-  for(i = 0; i < NUMBER_OF_THREADS; i++){
-    pthread_create(&thread[i], NULL, do_work,  (job_t*)pop(stack));
+  while(jobs->size != 0){
+    for(i = 0; i < NUMBER_OF_THREADS; i++){
+      pthread_create(&thread[i], NULL, do_work,  (job_t*)pop(stack));
 
+    }
+
+    for(i = 0; i < NUMBER_OF_THREADS; i++){
+      pthread_join(thread[i], NULL);
+    }
   }
 
-  for(i = 0; i < NUMBER_OF_THREADS; i++){
-    pthread_join(thread[i], NULL);
-  }
-
-
+  fclose(fp);
 }
 
 void *do_work(void *input)
@@ -47,9 +48,17 @@ void *do_work(void *input)
   // enough memory available
   else{
 
-    print_starting(fp, ((job_t*)input)->number);
-    print(fp, allocate, ((job_t*)input)->required_memory);
-    memory += ((job_t*)input)->required_memory;
+    while(((job_t*)input)->required_time > 0){
+      print_starting(fp, ((job_t*)input)->number);
+      print(fp, allocate, ((job_t*)input)->required_memory);
+      memory += ((job_t*)input)->required_memory;
+      ((job_t*)input)->required_time -= 2;
+      sleep(2);
+
+    }
+    print_completed(fp, ((job_t*)input)->number);
+    print(fp, deallocate, ((job_t*)input)->required_memory);
+    memory -= ((job_t*)input)->required_memory;
   }
 }
 
