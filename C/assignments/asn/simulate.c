71cf435af151a0f1a540dbd52a7341cf88b55e06
Kyle Cheung <kcheu4@uwo.ca>
2018-11-01 19:00:17 -0400

    updated simulate.c

--- C/assignments/asn/simulate.c
+++ C/assignments/asn/simulate.c
@@ -1,4 +1,4 @@
-/******************************************************************************
+/**********************************************************aaaaaaaasa********************
  *
  ******************************************************************************/
 
@@ -7,24 +7,32 @@
 int memory, max_memory;
 FILE *fp;
 linked_stack_t *jobs;
-char *allocate = "Allocating";
-char *deallocate = "Deallocating";
+char *allocate = "Allocating %d\n";
+char *deallocate = "Deallocating %d\n";
 
 void simulate(int memory_value, linked_stack_t *stack)
 {
   int i;
+  int k;
+  jobs = stack;
   max_memory = memory_value;
-  fp = fopen(SYSTEM_OUTPUT, "ab+");
+  memory = max_memory;
+  fp = fopen(SYSTEM_OUTPUT, "w");
   pthread_t thread[NUMBER_OF_THREADS];
 
   while(jobs->size != 0){
     for(i = 0; i < NUMBER_OF_THREADS; i++){
-      pthread_create(&thread[i], NULL, do_work,  (job_t*)pop(stack));
 
+      if (jobs->head != NULL){
+        pthread_create(&thread[i], NULL, do_work, pop(jobs));
+      }  
+      else{
+        break;
+      }
     }
 
-    for(i = 0; i < NUMBER_OF_THREADS; i++){
-      pthread_join(thread[i], NULL);
+    for( k = 0; k < i; k++){
+      pthread_join(thread[k], NULL);
     }
   }
 
@@ -40,25 +48,23 @@ void *do_work(void *input)
   }
 
   //insufficient memory, push onto stack
-  if((((job_t*)input)->required_memory + memory) > max_memory) {
-    print_insufficient_memory(fp, ((job_t*)input)->required_memory);
-    push(jobs, (job_t*)input);
+  if((((job_t*)input)->required_memory) > memory) {
+    print_insufficient_memory(fp, ((job_t*)input)->number);
+    push(jobs, input);
   }
 
   // enough memory available
   else{
 
-    while(((job_t*)input)->required_time > 0){
-      print_starting(fp, ((job_t*)input)->number);
-      print(fp, allocate, ((job_t*)input)->required_memory);
-      memory += ((job_t*)input)->required_memory;
-      ((job_t*)input)->required_time -= 2;
-      sleep(2);
+    print_starting(fp, ((job_t*)input)->number);
+    print(fp, allocate, ((job_t*)input)->required_memory);
+    sleep(((job_t*)input)->required_time);
+    memory -= ((job_t*)input)->required_memory;
+    ((job_t*)input)->required_time -= ((job_t*)input)->required_time;
 
-    }
     print_completed(fp, ((job_t*)input)->number);
     print(fp, deallocate, ((job_t*)input)->required_memory);
-    memory -= ((job_t*)input)->required_memory;
+    memory += ((job_t*)input)->required_memory;
   }
 }
 
